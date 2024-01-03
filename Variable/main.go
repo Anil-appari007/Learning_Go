f91cc8a1172af611bf7edc6708d6f94f8e06c481
anil-appari007 <anil.appari007@gmail.com>
2024-01-03 21:59:07 +0530

    Added Variables

---
+++ Variable/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string
+	a = "somedata"
+	fmt.Println("value of variable a is", a)
+
+	var b int = 10
+	fmt.Println("b = ", b)
+
+	c := 10.787
+	fmt.Println("c = ", c)
+
+	var d = true
+	fmt.Println("d = ", d)
+}
