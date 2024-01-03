bea5858e91cd5d863d111b9afd253731d8ee89a8
anil-appari007 <anil.appari007@gmail.com>
2024-01-03 22:08:24 +0530

    Added naming Convention

---
+++ NamingConvention/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	camelCaseVar := "camelCaseVar"
+	fmt.Println(camelCaseVar)
+
+	PascalCaseVar := "PascalCaseWord"
+	fmt.Println(PascalCaseVar)
+
+	snake_case_var := "snake_case_syntax"
+	fmt.Println(snake_case_var)
+}
